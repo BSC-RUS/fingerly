0e687d2180037d7961ddcc835923c533c4120c66
sergeybrazhnik <6epreu@gmail.com>
2018-02-14 16:44:43 +0300

    added constant IV to aes alg removed unused code

--- src/main/java/ru/bscmsk/fingerly/impl/android/AndroidTouchId.java
+++ src/main/java/ru/bscmsk/fingerly/impl/android/AndroidTouchId.java
@@ -13,29 +13,20 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
 import android.support.v4.os.CancellationSignal;
-import android.util.Base64;
-
-import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
+import ru.bscmsk.fingerly.TouchIdCodes;
+import ru.bscmsk.fingerly.impl.AbstractTouchId;
+import ru.bscmsk.fingerly.interfaces.UnionTouchIdCallback;
+import ru.bscmsk.fingerly.utils.AppLogger;
+import ru.bscmsk.fingerly.utils.IPrefsStore;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
-
-import ru.bscmsk.fingerly.TouchIdCodes;
-import ru.bscmsk.fingerly.impl.AbstractTouchId;
-import ru.bscmsk.fingerly.interfaces.UnionTouchIdCallback;
-import ru.bscmsk.fingerly.utils.AppLogger;
-import ru.bscmsk.fingerly.utils.IPrefsStore;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 import static android.content.Context.KEYGUARD_SERVICE;
 import static ru.bscmsk.fingerly.TouchIdCodes.FINGERPRINT_ERROR_CANCELED;
@@ -56,6 +47,7 @@ public class AndroidTouchId extends AbstractTouchId {
 	private KeyStore mKeyStore;
 	private KeyGenerator mKeyGenerator;
 	private CancellationSignal cancellationSignal;
+	private byte[] IV = new byte[] {0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1};
 
 	public AndroidTouchId(Context context, IPrefsStore prefs) {
 		super(context, prefs);
@@ -139,7 +131,6 @@ public class AndroidTouchId extends AbstractTouchId {
 		try {
 			mKeyStore.load(null);
 			mKeyStore.deleteEntry(TOUCH_ID_KEY);
-			removeIV();
 		} catch (IOException
 			| NoSuchAlgorithmException
 			| CertificateException
@@ -185,10 +176,9 @@ public class AndroidTouchId extends AbstractTouchId {
 			if (key == null)
 				return null;
 			if (mode == CipherMode.ENCRYPT) {
-				mCipher.init(Cipher.ENCRYPT_MODE, key);
-				saveIV(mCipher.getIV());
+				mCipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV));
 			} else {
-				mCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(loadIV()));
+				mCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV));
 			}
 		} catch (KeyPermanentlyInvalidatedException e) {
 			AppLogger.e(this, "KeyPermanentlyInvalidatedException ", e);
@@ -204,23 +194,6 @@ public class AndroidTouchId extends AbstractTouchId {
 			return null;
 		}
 		return mCipher;
-	}
-
-	private byte[] loadIV() {
-		try {
-			String base64Key = this.prefsStore.get(AES_IV, String.class, "");
-			return Base64.decode(base64Key, Base64.NO_WRAP);
-		} catch (Exception e) {
-			return null;
-		}
-	}
-
-	private void saveIV(byte[] bytes) {
-		prefsStore.add(AES_IV, Base64.encodeToString(bytes, Base64.NO_WRAP));
-	}
-
-	private void removeIV() {
-		prefsStore.remove(AES_IV);
 	}
 
 	private FingerprintManagerCompat.AuthenticationCallback getInternalCallBack(final UnionTouchIdCallback callback) {
